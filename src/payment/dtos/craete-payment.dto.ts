de581d2b947e8173fac2fdd6ab5230e4cdfd81b0
farid <f.esnaashari@drnext.ir>
2025-12-12 16:49:14 +0330

    chore(payment): add paid_at

--- src/payment/dtos/craete-payment.dto.ts
+++ src/payment/dtos/craete-payment.dto.ts
@@ -1,15 +1,17 @@
 import { z } from 'zod';
 import { PaymentCategory } from '../enums/payment-category.enum';
 import { Bank } from 'src/account/enums/bank.enum';
+import { dateDtoSchema } from 'src/common/zod-schemas/date.schema';
 
 export const createPaymentDtoSchema = z.object({
   price: z.number(),
-  bank: z.nativeEnum(Bank),
-  category: z.nativeEnum(PaymentCategory),
+  bank: z.enum(Bank),
+  category: z.enum(PaymentCategory),
   description: z.string().optional(),
   isFun: z.boolean(),
   isMaman: z.boolean(),
   ownerId: z.number(),
+  paidAt: dateDtoSchema,
 });
 
 export type CreatePaymentDto = z.infer<typeof createPaymentDtoSchema>;
